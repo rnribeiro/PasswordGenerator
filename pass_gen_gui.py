5abc1c2564eca5fbadbb00b49381ce6e81db90af
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2020-10-31 12:12:50 +0000

    Add files via upload

--- pass_gen_gui.py
+++ pass_gen_gui.py
@@ -1,33 +1,47 @@
-import rnribeiro as rr
+import tkinter as tk
 import clipboard
-window = rr.tk.Tk()
+window = tk.Tk()
 window.geometry('200x135')
 def copy():
     clipboard.copy(password)
+
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    frm_width = window.winfo_rootx() - window.winfo_x()
+    window_width = width + 2 * frm_width
+    height = window.winfo_height()
+    titlebar_height = window.winfo_rooty() - window.winfo_y()
+    window_height = height + titlebar_height + frm_width
+    x = window.winfo_screenwidth() // 2 - window_width // 2
+    y = window.winfo_screenheight() // 2 - window_height // 2
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    window.deiconify()
+
 def newpassword():
     global password_label
     global password
     lenght = int(pass_lenght_input.get())
     if lenght < 4:
-        rr.messagebox.showinfo('Error!', 'Password lenght is too short\nTry again!')
-        pass_lenght_input.delete(0, rr.tk.END)
+        messagebox.showinfo('Error!', 'Password lenght is too short\nTry again!')
+        pass_lenght_input.delete(0, tk.END)
         pass_lenght_input.delete(0, '')
     else:
-        chars = rr.string.ascii_letters + rr.string.digits + rr.string.punctuation
-        password = ''.join(rr.random.sample(chars , lenght))
+        chars = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.sample(chars , lenght))
         pass_label.configure(text = password)
 
-pass_lenght_label = rr.tk.Label(window, text = 'Enter password lenght:')
+pass_lenght_label = tk.Label(window, text = 'Enter password lenght:')
 pass_lenght_label.pack()
-pass_lenght_input = rr.tk.Entry(window)
+pass_lenght_input = tk.Entry(window)
 pass_lenght_input.pack()
-pass_generator_button = rr.tk.Button(window, text = 'Generate', command = newpassword)
+pass_generator_button = tk.Button(window, text = 'Generate', command = newpassword)
 pass_generator_button.pack()
-password_label = rr.tk.Label(window, text = 'Password:')
+password_label = tk.Label(window, text = 'Password:')
 password_label.pack()
-pass_label = rr.tk.Label(window, text = '')
+pass_label = tk.Label(window, text = '')
 pass_label.pack()
-copy_button = rr.tk.Button(window, text = 'Copy Password', command = copy)
+copy_button = tk.Button(window, text = 'Copy Password', command = copy)
 copy_button.pack()
-rr.center_window(window)
+center_window(window)
 window.mainloop()
--- pass_gen_terminal.py
+++ pass_gen_terminal.py
@@ -1,11 +1,12 @@
-import rnribeiro as rr
+import string
+import random
 import clipboard
 def newpassword(lenght):
     if lenght < 4:
         print('Error!', 'Password lenght is too short\nTry again!')
     else:
-        chars = rr.string.ascii_letters + rr.string.digits + rr.string.punctuation
-        password = ''.join(rr.random.sample(chars , lenght))
+        chars = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.sample(chars , lenght))
         print(password)
         if input('Copy password (y/n): ') == 'y':
             clipboard.copy(password)
